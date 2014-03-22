4b0d5ee13061e34b2ec93a0e9d405e7eef7c281d
Alex <vap43@hotmail.com>
2014-03-22 15:33:07 -0400

    Rewrote occupancy grid for speed

--- Alex/getOccupancyGrid.m
+++ Alex/getOccupancyGrid.m
@@ -1,8 +1,8 @@
 function [Occ,Known,gr_x,gr_y] = getOccupancyGrid(X,Y,Z,PARAMS)
 
-XY_RESOLUTION = PARAMS.XY_RESOLUTION; % 5 cm
-ROBOT_HEIGHT = PARAMS.ROBOT_HEIGHT; % 40 cm
-GROUND_HEIGHT = PARAMS.GROUND_HEIGHT; % 5 cm
+XY_RESOLUTION = PARAMS.XY_RESOLUTION;
+ROBOT_HEIGHT = PARAMS.ROBOT_HEIGHT;
+GROUND_HEIGHT = PARAMS.GROUND_HEIGHT;
 
 dom = [floor(min(X)/XY_RESOLUTION) ceil(max(X)/XY_RESOLUTION)] * XY_RESOLUTION;
 rng = [floor(min(Y)/XY_RESOLUTION) ceil(max(Y)/XY_RESOLUTION)] * XY_RESOLUTION;
@@ -13,20 +13,35 @@ gr_y = rng(1):XY_RESOLUTION:rng(2);
 Occ = int8(zeros(length(gr_y)-1,length(gr_x)-1));
 Known = Occ;
 
-for i = 1:length(Z),
-    
-    if GROUND_HEIGHT < Z(i) && Z(i) < ROBOT_HEIGHT, % Case: Occupied
-        
-        x = floor( (X(i) - dom(1)) / XY_RESOLUTION ) + 1;
-        y = floor( (Y(i) - rng(1)) / XY_RESOLUTION ) + 1;
-        Known(y,x) = Known(y,x) + 1;
-        Occ(y,x) = Occ(y,x) + 1;
-        
-    elseif Z(i) < GROUND_HEIGHT, % Case: Not Occupied
-        
-        x = floor( (X(i) - dom(1)) / XY_RESOLUTION ) + 1;
-        y = floor( (Y(i) - rng(1)) / XY_RESOLUTION ) + 1;
-        Known(y,x) = Known(y,x) + 1;
-        
-    end
-end
+% for i = 1:length(Z),
+%     
+%     if GROUND_HEIGHT < Z(i) && Z(i) < ROBOT_HEIGHT, % Case: Occupied
+%         
+%         x = floor( (X(i) - dom(1)) / XY_RESOLUTION ) + 1;
+%         y = floor( (Y(i) - rng(1)) / XY_RESOLUTION ) + 1;
+%         Known(y,x) = Known(y,x) + 1;
+%         Occ(y,x) = Occ(y,x) + 1;
+%         
+%     elseif Z(i) < GROUND_HEIGHT, % Case: Not Occupied
+%         
+%         x = floor( (X(i) - dom(1)) / XY_RESOLUTION ) + 1;
+%         y = floor( (Y(i) - rng(1)) / XY_RESOLUTION ) + 1;
+%         Known(y,x) = Known(y,x) + 1;
+%         
+%     end
+% end
+
+AllInd = 1:length(Z);
+OccLgc = GROUND_HEIGHT < Z & Z < ROBOT_HEIGHT;
+NonOccLgc = Z < GROUND_HEIGHT;
+OccInd = AllInd(OccLgc);
+NonOccInd = AllInd(NonOccLgc);
+x = floor( (X(OccInd) - dom(1)) / XY_RESOLUTION ) + 1;
+y = floor( (Y(OccInd) - rng(1)) / XY_RESOLUTION ) + 1;
+s = sub2ind(size(Occ),y,x);
+for i = 1:length(s), Known(s(i)) = Known(s(i)) + 1; end
+for i = 1:length(s), Occ(s(i)) = Occ(s(i)) + 1; end
+x = floor( (X(NonOccInd) - dom(1)) / XY_RESOLUTION ) + 1;
+y = floor( (Y(NonOccInd) - rng(1)) / XY_RESOLUTION ) + 1;
+s = sub2ind(size(Occ),y,x);
+for i = 1:length(s), Known(s(i)) = Known(s(i)) + 1; end
